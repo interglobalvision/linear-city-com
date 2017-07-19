58d444f5ddaf8c5e29c7f3d67ba9ae29dc853309
cas <cas@interglobal.vision>
2017-07-18 19:47:15 -0500

    add press quotes post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -62,6 +62,50 @@ function register_cpt_property() {
   register_post_type( 'property', $args );
 }
 
+add_action( 'init', 'register_cpt_press_quote' );
+
+function register_cpt_press_quote() {
+
+  $labels = array(
+    'name' => _x( 'Press Quotes', 'press_quote' ),
+    'singular_name' => _x( 'Press Quote', 'press_quote' ),
+    'add_new' => _x( 'Add New', 'press_quote' ),
+    'add_new_item' => _x( 'Add New Press Quote', 'press_quote' ),
+    'edit_item' => _x( 'Edit Press Quote', 'press_quote' ),
+    'new_item' => _x( 'New Press Quote', 'press_quote' ),
+    'view_item' => _x( 'View Press Quote', 'press_quote' ),
+    'search_items' => _x( 'Search Press Quotes', 'press_quote' ),
+    'not_found' => _x( 'No press quotes found', 'project' ),
+    'not_found_in_trash' => _x( 'No press quotes found in Trash', 'project' ),
+    'parent_item_colon' => _x( 'Parent Press Quote:', 'press_quote' ),
+    'menu_name' => _x( 'Press Quotes', 'press_quote' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'press_quote', $args );
+}
+
+
 add_action( 'init', 'register_cpt_project' );
 
 function register_cpt_project() {
